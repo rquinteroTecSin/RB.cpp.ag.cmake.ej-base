94c5a8f42032321f4b8e5fd4f2fef0c99fea3331
Ricardo Quintero <55771796+rquinteroTecSin@users.noreply.github.com>
2024-12-23 20:10:47 +0000

    Añadir funciones de resta y división en student.cpp y student.hpp

--- student.cpp
+++ student.cpp
@@ -12,3 +12,18 @@ int mul(int a, int b)
 {
    return a * b; // TODO: fix
 }
+
+// Exercise 3: implements function 'sub', that subtracts two numbers
+int sub(int a, int b)
+{
+   return a - b;
+}
+
+// Exercise 4: implements function 'div', that divides two numbers, returns 0 if divisor is 0
+int div(int a, int b)
+{
+   if (b == 0) {
+      return 0;
+   }
+   return a / b;
+}
